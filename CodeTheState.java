028d64a2322c48da21115cbe2427357a8e6aefc2
makrinantz <t8240097@aueb.gr>
2025-11-10 18:03:32 +0200

    add a variable

--- CodeTheState.java
+++ CodeTheState.java
@@ -14,10 +14,12 @@ public class CodeTheState {
     int w = 2;
 >>>>>>> 4cbc6864b36813f06c0ce3223b04186cbcaf1492
     int d = 88;
+    int mak = 100;
 }
 =======
     int a = 3;
     int p = 4;
+    
 
 }
 >>>>>>> 90c3ab1c97e7b92bee36cc3bc64bc2f89b409512
