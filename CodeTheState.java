a7baece090a40326973a28decc6431a6d3a4b83a
xarisxatzifotiadis <t8240168@aueb.gr>
2025-11-10 18:12:30 +0200

    Update CodeTheState.java

--- CodeTheState.java
+++ CodeTheState.java
@@ -1,23 +1,4 @@
 public class CodeTheState {
 
-    int x = 5;
-    int y = 3;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    int r = 2;
-=======
-    int k = 4;
-    int z = 5;
->>>>>>> f8fe1313c0d3f2ab0cc465c1b10b68177d4e9dab
-=======
-    int w = 2;
->>>>>>> 4cbc6864b36813f06c0ce3223b04186cbcaf1492
-    int d = 88;
 }
-=======
-    int a = 3;
-    int p = 4;
 
-}
->>>>>>> 90c3ab1c97e7b92bee36cc3bc64bc2f89b409512
