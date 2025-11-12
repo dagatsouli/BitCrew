53f346d801b676eacc3a40e5574435f8a4b00204
makrinantz <t8240097@aueb.gr>
2025-11-12 16:38:07 +0200

    arxikos kvdikas

--- EducationBudget.java
+++ EducationBudget.java
@@ -4,8 +4,6 @@ public class EducationBudget {
         double taktikos = 5594000000.0;        
         double dimosiesEpendyseis = 1012000000.0;
         double synolo = 6606000000.0;
-
-        // Display in console
         System.out.println("Ministry of Education, Religious Affairs and Sports");
         System.out.println("--------------------------------------------------");
         System.out.println("Regular Budget: " + taktikos + " €");
