4a6e1a1d406fe411dd8715dae2447f153216ea87
danaegatsouli <dagatsouli@gmail.com>
2025-11-12 15:57:43 +0200

    1

--- CodeTheState.java
+++ CodeTheState.java
@@ -1,15 +1,15 @@
 public class CodeTheState {
     public static void main(String[] args) {
-        String message = "Τι αποφάσισε ο πρωθυπουργός σήμερα; Η χώρα , σας περιμένει να πατήσετε έναρξη."΄
+        String message = "The Prime Minister's decision is today! Your country is waiting for you to press start.";
         System.out.println(message);
-        System.out.println("\n Πατήστε Εnter για έναρξη..")΄;
+        System.out.println("\nPress Enter to start...");
+
         try {
             System.in.read();
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
-        System.out.println("Έναρξη επιβεβαιώθηκε. Εκκίνηση διαδικασιών..");
+
+        System.out.println("Startup confirmed. Initiating procedures...");
     }
-
-
 }
