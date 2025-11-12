28a138ef2cdbfba502e71ad15bfdcb0a0b57d755
makrinantz <t8240097@aueb.gr>
2025-11-12 16:17:50 +0200

    add a vairable

--- CodeTheState.java
+++ CodeTheState.java
@@ -1,15 +1,1 @@
-public class CodeTheState {
-    public static void main(String[] args) {
-        String message = "The Prime Minister's decision is today! Your country is waiting for you to press start.";
-        System.out.println(message);
-        System.out.println("\nPress Enter to start...");
 
-        try {
-            System.in.read();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Startup confirmed. Initiating procedures...");
-    }
-}
