ae4f012e852f3fee306722404041509c1550ff5a
Marielenapap <t8240108@aueb.gr>
2025-11-12 16:49:24 +0200

    1

---
+++ MinistryHealth.java
@@ -0,0 +1,12 @@
+public class MinistryHealth {
+    public static void main(String[] args) {
+        long RegularBudget = 6_608_424_000L;
+        long PublicinvestmentBudget =  569_000_000L;
+        long Sum = RegularBudget + PublicinvestmentBudget;
+        System.out.println(''MinistryHealth);
+        System.out.println(''Regural Budget:'' + RegularBudget + ''€'');
+        System.out.println(''Public Investment Budget:" + PublicinvestmentBudget + ''€")
+        System.out.println(''Sum:'' + sum + ''€'');
+    }
+}
+
