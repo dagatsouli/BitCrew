44e08cfe14ecf286f3080852a48d89a27e00b320
xarisxatzifotiadis <t8240168@aueb.gr>
2025-11-12 15:36:23 +0200

    arxiko mhnyma

--- CodeTheState.java
+++ CodeTheState.java
@@ -1,3 +1,15 @@
 public class CodeTheState {
+    public static void main(String[] args) {
+        String message = "Τι αποφάσισε ο πρωθυπουργός σήμερα; Η χώρα , σας περιμένει να πατήσετε έναρξη."΄
+        System.out.println(message);
+        System.out.println("\n Πατήστε Εnter για έναρξη..")΄;
+        try {
+            System.in.read();
+        } catch (Exception e) {
+
+        }
+        System.out.println("Έναρξη επιβεβαιώθηκε. Εκκίνηση διαδικασιών..");
+    }
+
 
 }
