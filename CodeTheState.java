ffc3a8d60073abfbd02b061d74cf9e67c62ebb59
Marielenapap <t8240108@aueb.gr>
2025-11-12 16:21:16 +0200

    Merge branch 'main' of https://github.com/dagatsouli/BitCrew

--- CodeTheState.java
+++ CodeTheState.java
@@ -1,10 +1,28 @@
 public class CodeTheState {
+    public static void main(String[] args) {
+        String message = "The Prime Minister's decision is today! Your country is waiting for you to press start.";
+        System.out.println(message);
+        System.out.println("\nPress Enter to start...");
 
     int x = 5;
     int y = 3;
+<<<<<<< Updated upstream
     int r = 2;
     int k = 4;
     int z = 5;
     int w = 2;
     int x = 0;
 }
+=======
+    int w = 7;
+}
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Startup confirmed. Initiating procedures...");
+    }
+
+>>>>>>> Stashed changes
