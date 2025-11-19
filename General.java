75c94d0c3fb8ae7ebee765587855a10b30232e1d
danaegatsouli <dagatsouli@gmail.com>
2025-11-19 14:54:15 +0200

    ekana to general gia ta esoda

---
+++ General.java
@@ -0,0 +1,22 @@
+public class General {
+    
+    public static void main(String[] args) {
+
+    System.out.println("The total revenues of Greece, based on the state budget for 2025, amount to 1,304,827,000,000 \u20AC . ");
+    long taxes = 62055000000L;
+    long socialContributions = 60000000;
+    long transfers = 8131000000L;
+    long salesOfgoods = 2405000000L;
+    long otherCurrentRevenues = 2775000000L;
+    long fixedAssets = 37000000;
+    long debtSecurities2 = 11000000;
+    long loans = 20000000;
+    long equitySecuritiesAndInvestment = 467000000;
+    long LiabilitiesfromCurrency = 66000000;
+    long debtSecurities = 24973000000L;
+    long loans2 = 1202027000000L;
+    long financialDerivatives = 800000000;
+
+    System.out.println(loans2);
+    }
+}
