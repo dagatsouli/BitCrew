aed64872c74a68973061bd24640ddfcce9b80d49
danaegatsouli <dagatsouli@gmail.com>
2025-11-20 13:22:07 +0200

    ESODA-EKSODA

--- General.java
+++ General.java
@@ -2,7 +2,9 @@ public class General {
     
     public static void main(String[] args) {
 
-    System.out.println("The total revenues of Greece, based on the state budget for 2025, amount to 1,304,827,000,000 \u20AC . ");
+    System.out.println("The total revenues of Greece, based on the state budget for 2025, amount to 1,304,827,000,000 EUR . ");
+    System.out.println("The total expenditures of Greece, based on the state budget for 2025, amount to 1,307,907,506,000 EUR.");
+
     long taxes = 62055000000L;
     long socialContributions = 60000000;
     long transfers = 8131000000L;
@@ -16,7 +18,21 @@ public class General {
     long debtSecurities = 24973000000L;
     long loans2 = 1202027000000L;
     long financialDerivatives = 800000000;
-
-    System.out.println(loans2);
+    long employeeBenefits = 14889199000L;
+    long socialBenefits = 425136000L;
+    long transfersExpenses = 34741365000L;
+    long purchasesGoodsServices = 2039542000L;
+    long subsidies = 80630000L;
+    long interest = 7701101000L;
+    long otherExpenses = 101553000L;
+    long allocatedCredits = 17283053000L;
+    long fixedAssetsExpenses = 2609600000L;
+    long valuables = 85000L;         
+    long loansExpenses = 3741000000L;
+    long equitySecuritiesExpenses = 1755112000L;
+    long debtSecuritiesExpenses = 19375000000L;
+    long loans2Expenses = 1203165130000L;    
+    
     }
+
 }
