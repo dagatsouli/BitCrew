302acefe20c30ef1174d1ab644759ee0809dd2f4
danaegatsouli <dagatsouli@gmail.com>
2025-11-24 11:53:44 +0200

    EKANA KAI TA EKSODA

--- General.java
+++ General.java
@@ -6,12 +6,9 @@ public class General {
     
     Scanner input = new Scanner(System.in);
 
-<<<<<<< Updated upstream
     System.out.println("The total revenues of Greece, based on the state budget for 2025, amount to 1,304,827,000,000 EUR .");
     System.out.println("The total expenditures of Greece, based on the state budget for 2025, amount to 1,307,907,506,000 EUR.");
     
-=======
->>>>>>> Stashed changes
     //REVENUES
     long taxes = 62055000000L;
     long socialContributions = 60000000;
@@ -40,23 +37,12 @@ public class General {
     long debtSecuritiesExpenses = 19375000000L;
     long loans2Expenses = 1203165130000L;    
 
-<<<<<<< Updated upstream
     System.out.print("\nDo you want to see the detailed version of the Revenues? (yes/no): ");
     String answer = input.nextLine().trim().toLowerCase();
 
 
      if (answer.equals("yes")) {
             System.out.println("\n----- Detailed Version of the Revenues -----");
-=======
-    System.out.println("The total revenues of Greece, based on the state budget for 2025, amount to 1,304,827,000,000 EUR .");
-    System.out.println("The total expenditures of Greece, based on the state budget for 2025, amount to 1,307,907,506,000 EUR.");
-
-    System.out.print("\nDo you want to see the detailed version of the Revenues? (yes/no): ");
-    String answer = input.nextLine().trim().toLowerCase();
-
-     if (answer.equals("yes")) {
-            System.out.println("\n----- Detailed Version of the Expenses -----");
->>>>>>> Stashed changes
             System.out.println("Taxes: " + taxes + " EUR");
             System.out.println("Social contributions: " + socialContributions + " EUR");
             System.out.println("Transfers: " + transfers + " EUR");
@@ -76,31 +62,28 @@ public class General {
 
     System.out.print("\nDo you want to see the detailed version of the Expenses? (yes/no): ");
     String answer2 = input.nextLine().trim().toLowerCase();
-
-<<<<<<< Updated upstream
-    }
-}
-=======
-        if (answer.equals("yes")) {
-            System.out.println("\n----- Detailed Version of the Revenues -----");
-            System.out.println("Employee Benefits: " + employeeBenefits + " EUR");
-            System.out.println("Social Benefits: " + socialBenefits + " EUR");
-            System.out.println("Transfers Expences: " + transfersExpences + " EUR");
+    
+    if (answer2.equals("yes")) {
+            System.out.println("\n----- Detailed Version of the Expenses -----");
+            System.out.println("Employee benefits: " + employeeBenefits + " EUR");
+            System.out.println("Social benefits: " + socialBenefits + " EUR");
+            System.out.println("Transfers: " + transfersExpenses + " EUR");
             System.out.println("Purchases of goods and services: " + purchasesGoodsServices + " EUR");
             System.out.println("Subsidies: " + subsidies + " EUR");
             System.out.println("Interest: " + interest + " EUR");
-            System.out.println("Other Expences: " + otherExpenses + " EUR");
-            System.out.println("Allocated Credits: " + allocatedCredits + " EUR");
-            System.out.println("Fixed Assets Expenses: " + fixedAssetsExpenses + " EUR");
+            System.out.println("Other expenses: " + otherExpenses + " EUR");
+            System.out.println("Allocated credits: " + allocatedCredits + " EUR");
+            System.out.println("Fixed assets: " + fixedAssetsExpenses + " EUR");
             System.out.println("Valuables: " + valuables + " EUR");
-            System.out.println("Loans Expenses: " + loansExpenses + " EUR");
+            System.out.println("Loans: " + loansExpenses + " EUR");
+            System.out.println("Equity Securities Expenses: " + equitySecuritiesExpenses + " EUR");
+            System.out.println("Debt Securities Expenses: " + debtSecuritiesExpenses + " EUR");
+            System.out.println("Loans 2 Expenses: " + loans2Expenses + " EUR");
             System.out.println("--------------------------------------------------");
         } else {
-            System.out.println("\n OK. No detailed Regular Budget will be displayed.");
+            System.out.println("\nOK. No detailed Expenses will be displayed.");
         }
 
+        input.close();
     }
 }
-
-
->>>>>>> Stashed changes
