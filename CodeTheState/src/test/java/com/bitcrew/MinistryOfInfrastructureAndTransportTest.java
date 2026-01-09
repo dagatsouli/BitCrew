367ee533307c50d90e326f030a47d68962ecc12d
makrinantz <t8240097@aueb.gr>
2026-01-09 13:45:54 +0200

    diorthvsh

--- CodeTheState/src/test/java/com/bitcrew/MinistryOfInfrastructureAndTransportTest.java
+++ CodeTheState/src/test/java/com/bitcrew/MinistryOfInfrastructureAndTransportTest.java
@@ -1,7 +1,6 @@
 package com.bitcrew;
  import org.junit.jupiter.api.Test; 
  import static org.junit.jupiter.api.Assertions.assertEquals; 
- import static org.junit.jupiter.api.Assertions.assertEquals; 
  public class MinistryOfInfrastructureAndTransportTest { 
     @Test
     public void testConstructorInitializesValuesCorrectly() { 
